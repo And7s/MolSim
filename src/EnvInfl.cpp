2e027b4f0cea7e70fd44e2b38b6b96c0ba6f5de6
paul <paul@paulpc.(none)>
2013-12-10 10:15:05 +0100

    Environmental Influces first attempt

---
+++ src/EnvInfl.cpp
@@ -0,0 +1,57 @@
+/*
+ * EnvironmentalInfluences.cpp
+ *
+ *  Created on: Dec 8, 2013
+ *      Author: paul
+ */
+
+#include "EnvInfl.h"
+
+LoggerPtr loggerEnv(Logger::getLogger("main.env"));
+
+EnvInfl* EnvInfl::instance = NULL;
+
+EnvInfl::EnvInfl() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EnvInfl::~EnvInfl() {
+	// TODO Auto-generated destructor stub
+}
+
+EnvInfl* EnvInfl::getInstance(){
+	if(instance == NULL){
+		instance = new EnvInfl();
+	}
+	return instance;
+}
+
+void EnvInfl::destroy(){
+	if(instance != NULL){
+		delete instance;
+	}
+	instance = NULL;
+}
+
+void EnvInfl::setG(double g){
+	this->g = g;
+}
+
+double EnvInfl::getG(){
+	return this->g;
+}
+
+/**
+ * gravity in -y direction
+ */
+void EnvInfl::calculateGravity(Particle* part){
+	utils::Vector<double,3> force;
+	force[0] = 0;
+	force[1] = -1;
+	force[2] = 0;
+	force[1] *= part->getM() * this->g;
+	part->addOnF(force);
+}
+
+
---
+++ src/EnvInfl.h
@@ -0,0 +1,63 @@
+/*
+ * EnvironmentalInfluences.h
+ *
+ *  Created on: Dec 8, 2013
+ *      Author: paul
+ */
+
+#ifndef ENVINFL_H_
+#define ENVINFL_H_
+
+#include "Particle.h"
+
+#include <log4cxx/logger.h>
+#include <log4cxx/xml/domconfigurator.h>
+
+using namespace log4cxx;
+using namespace log4cxx::xml;
+using namespace log4cxx::helpers;
+
+/**
+ * this class provides general environmental influences on the particles. Most likely, those forces do not depend on the location of the particle,
+ * since they affect the whole space evenly.
+ * Forces like gravity, wind, vortexes etc may defined here.
+ * The class should only exist once, but reachable from anywhere, so it is a singleton.
+ */
+class EnvInfl {
+private:
+	/**
+	 * singleton
+	 */
+	EnvInfl();
+	virtual ~EnvInfl();
+
+	/**
+	 * instance
+	 */
+	static EnvInfl* instance;
+
+public:
+	static EnvInfl* getInstance();
+
+	static void destroy();
+
+	/**
+	 * gravity acceleration
+	 */
+	double g;
+
+	/**
+	 * set the gravity acceleration
+	 */
+	void setG(double g);
+
+	/**
+	 * get the gravity acceleration
+	 */
+	double getG();
+
+	void calculateGravity(Particle* parts);
+
+};
+
+#endif /* ENVINFL */
